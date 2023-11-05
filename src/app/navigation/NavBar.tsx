78197bb90d6ab7be6652b51f8d408a2e73598ee6
KeinoC <keino.chichester@gmail.com>
2023-11-05 11:25:46 -0500

    added logo on navbar

--- src/app/navigation/NavBar.tsx
+++ src/app/navigation/NavBar.tsx
@@ -1,8 +1,10 @@
-'use client'
+"use client";
 import React from "react";
 import { logout } from "@/services/auth.service";
 import { User } from "@/models/user.model";
 import { usePathname, useRouter } from "next/navigation";
+// import {logo} from '../public/tube-queue-svgrepo-com.svg'
+import Image from "next/image";
 
 interface Props {
     currentUser: User | null;
@@ -10,50 +12,55 @@ interface Props {
 
 export default function NavBar({ currentUser }: Props) {
     const pathname = usePathname().split("/")[0];
-    const router = useRouter()
+    const router = useRouter();
 
-    const handleClick: React.MouseEventHandler<HTMLButtonElement> | undefined = async  () => {
-
+    const handleClick:
+        | React.MouseEventHandler<HTMLButtonElement>
+        | undefined = async () => {
         const navToLogin = () => {
             router.push("/login");
-        }
+        };
 
         if (currentUser) {
             await logout();
         } else {
             router.push("/login");
         }
-        if(!currentUser) {
+        if (!currentUser) {
             await navToLogin();
         }
     };
 
-
-
     return (
         <div
             className={`${
-                !currentUser && "flex flex-row justify-center items-center"
+                !currentUser && "flex flex-row items-center"
             } w-full flex flex-row justify-end items-center text-lg font-thin text-slate-300 border-red-500`}
         >
-            
 
-            {currentUser && (
-                <div>
-                <span>Welcome back </span>
-                <span className="font-bold">
-                    {currentUser?.name.split(" ")[0]}!!{" "}
-                </span>
-            </div>
-            )}
+            <div onClick={()=>router.push('/')} className='flex  flex-grow justify-start items-center  text-2xl text-slate-300 px-10 '>
+                <Image width={70} height={150} alt='logo' className=' p-2 object-cover' src='./line-up-single-line-svgrepo-com.svg'  />
+                <p className="relative right-5 font-extrabold">UP</p>
+                </div>
+            <div className='w-[90vw] flex flex-row justify-end items-center'>
+                {currentUser && (
+                    <div>
+                        <span>Welcome back </span>
+                        <span className="font-bold">
+                            {currentUser?.name.split(" ")[0]}!!{" "}
+                        </span>
+                    </div>
+                )}
 
                 {pathname !== "login" && (
                     <button
                         onClick={handleClick}
-                        className='relative px-5 rounded-full m-5 border-[1px] border-purple-600  hover:bg-purple-600 text-slate-300 text-2xl hover:font-semibold'>
-                        {currentUser ? 'Logout' : 'Login'}
+                        className="relative px-5 rounded-full m-5 border-[1px] border-purple-600  hover:bg-purple-600 text-slate-300 text-2xl hover:font-semibold"
+                    >
+                        {currentUser ? "Logout" : "Login"}
                     </button>
                 )}
+            </div>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <NavBar currentUser={null}  />
-      HOME
+      <h1> Welcome</h1>
     </main>
   )
 }
